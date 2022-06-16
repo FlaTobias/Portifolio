dfd40ca270f3029f7e60cee1ba51868e186b225f
FlaTobias <flavioa.tobias@gmail.com>
2022-06-16 01:23:20 -0300

    debug

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import './App.scss'
 
 const App = () => {
   return (
+    <>
       <Routes>
-          <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
+        </Route>
       </Routes>
+    </>
   )
 }
 
