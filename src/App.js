f1e23398f214663eb032d123d357f93c6c3e949f
FlaTobias <flavioa.tobias@gmail.com>
2022-06-16 01:21:05 -0300

    Debug3

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.scss'
 const App = () => {
   return (
       <Routes>
+          <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
