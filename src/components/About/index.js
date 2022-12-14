704a486f65c6ad365d2199b643e1aa2d862a4719
FlaTobias <flavioa.tobias@gmail.com>
2022-12-14 17:11:12 -0300

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -52,16 +52,16 @@ const About = () => {
         <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
-              <FontAwesomeIcon icon={fasalesforce} color="#DD0031" />
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
             <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+             <FontAwesomeIcon icon={faSalesforce} color="#009EDB" />
             </div>
             <div className="face3">
               <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
             </div>
             <div className="face4">
-              <FontAwesomeIcon icon={fareact} color="#5ED4F4" />
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
             </div>
             <div className="face5">
               <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
